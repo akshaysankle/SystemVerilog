3450c15b0838009ecfe15ceef6d80dd27ca919f6
Akshay Sankle <32200419+akshaysankle@users.noreply.github.com>
2024-05-18 22:38:41 +0530

    Update mod_10.sv

--- mod_10.sv
+++ mod_10.sv
@@ -1,17 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 18.05.2024 19:11:52
+// Design Name:
+// Module Name: mod_10
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module mod_10(    output reg C , input wire clk, input reg rst);
-    reg [7:0]cnt;
-    always @(posedge clk) begin
-    
-    if(!rst && cnt < 10) begin
-    C = cnt;
-   cnt++; 
+  reg [7:0]cnt;
+  always @(posedge clk)
+  begin
+
+    if(!rst && cnt < 10)
+    begin
+      C = cnt;
+      cnt++;
     end
-   
-    else begin
-    cnt = 0;
+
+    else
+    begin
+      cnt = 0;
     end
-    
-   
-    end
+
+
+  end
 
 endmodule
